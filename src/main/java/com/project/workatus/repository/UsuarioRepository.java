584be347232903f7c87b2ee8b9927fd1cef91b74
calexs <leopardotm@outlook.com>
2022-05-03 22:14:12 -0300

    (add): UsuarioRepository.java

---
+++ src/main/java/com/project/workatus/repository/UsuarioRepository.java
@@ -0,0 +1,12 @@
+package com.project.workatus.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.project.workatus.model.UsuarioModel;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<UsuarioModel, Integer> {
+
+    public Optional<UsuarioModel> findByLogin(String login);
+}
