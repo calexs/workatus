1f35950f764a74f6737d3329f831c863e19e3dfe
calexs <leopardotm@outlook.com>
2022-05-03 22:12:56 -0300

    (add) WorkatusApplication.java

---
+++ src/main/java/com/project/workatus/WorkatusApplication.java
@@ -0,0 +1,13 @@
+package com.project.workatus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkatusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkatusApplication.class, args);		
+	}
+
+}
